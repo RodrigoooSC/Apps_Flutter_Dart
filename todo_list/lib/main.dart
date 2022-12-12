04facf7f02e42a85d11b3a4ca519e78e61e445c9
RodrigoooSC <rodrigooosc@gmail.com>
2022-12-12 17:41:10 -0300

    Developing project todo_list

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/screens/home.dart';
+import 'package:todo_list/theme/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Todo List",
+    home: Home(),
+    themeMode: ThemeMode.system,
+    theme: lightTheme(),
+    darkTheme: darkTheme(),
+  ));
 }
